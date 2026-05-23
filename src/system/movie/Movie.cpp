5e898d542a2c0b0a6cec51b44c17d4f0aafea9b2
freeqaz <me@freeqaz.com>
2026-05-23 02:59:24 +0000

    decomp: Movie::Impl::MsPerFrame 0% -> 96%

--- src/system/movie/Movie.cpp
+++ src/system/movie/Movie.cpp
@@ -180,6 +180,28 @@ void Movie::Impl::SetWidthHeight(int w, int h) {
     mHeight = h;
 }
 
+float Movie::Impl::MsPerFrame() const {
+    bool ok = true;
+    if (mThreadId != (unsigned int)OSGetCurrentThread()) {
+        unsigned int tid = mThreadId;
+        bool b = false;
+        if (tid == kNoThread) {
+            bool main = true;
+            if (gMainThreadID != 0 && gMainThreadID != OSGetCurrentThread()) main = false;
+            if (main) b = true;
+        }
+        if (!b) ok = false;
+    }
+    MILO_ASSERT(ok, 0x634);
+    float ms;
+    if (mBink != NULL) {
+        ms = 1000.0f * (float)mBink->FrameRateDiv / (float)mBink->FrameRate;
+    } else {
+        ms = 1000.0f;
+    }
+    return ms;
+}
+
 int Movie::Impl::NextFrame() {
     bool ok = true;
     if (mThreadId != (unsigned int)OSGetCurrentThread()) {
