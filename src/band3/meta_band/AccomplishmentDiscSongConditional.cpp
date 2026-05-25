628fd39f4d5a56a5a76dadafc596b735d64f0c59
freeqaz <me@freeqaz.com>
2026-05-25 10:54:14 +0000

    decomp: AccomplishmentDiscSongConditional InqSongs stable_sort -> 100% via _Temporary_buffer<Symbol*, Symbol> spec

--- src/band3/meta_band/AccomplishmentDiscSongConditional.cpp
+++ src/band3/meta_band/AccomplishmentDiscSongConditional.cpp
@@ -9,8 +9,33 @@
 #include "os/Debug.h"
 #include "meta_band/BandSongMgr.h"
 #include "stl/_algo.h"
+#include "utl/MemMgr.h"
 #include "utl/Symbols.h"
 #include <vector>
+
+namespace stlpmtx_std {
+
+template <>
+inline void _Temporary_buffer<Symbol *, Symbol>::_M_allocate_buffer() {
+    _M_original_len = _M_len;
+    _M_buffer = 0;
+    if (_M_len > (ptrdiff_t)(INT_MAX / sizeof(Symbol)))
+        _M_len = INT_MAX / sizeof(Symbol);
+    while (_M_len > 0) {
+        _M_buffer = (Symbol *)_MemAlloc(_M_len * sizeof(Symbol), 0);
+        if (_M_buffer)
+            break;
+        _M_len /= 2;
+    }
+}
+
+template <>
+inline _Temporary_buffer<Symbol *, Symbol>::~_Temporary_buffer() {
+    _STLP_STD::_Destroy_Range(_M_buffer, _M_buffer + _M_len);
+    _MemFree(_M_buffer);
+}
+
+} // namespace stlpmtx_std
 
 AccomplishmentDiscSongConditional::AccomplishmentDiscSongConditional(DataArray *arr, int i)
     : AccomplishmentSongConditional(arr, i), mPartDifficultySym("") {
