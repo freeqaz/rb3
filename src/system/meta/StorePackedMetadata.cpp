5c560168056a5c1146e53b08db780fd2fa5911c0
freeqaz <me@freeqaz.com>
2026-05-26 18:11:33 +0000

    decomp: StoreContentStateCache::UpdateContentStateFromFastEnum 97.7->97.8%
    permuter sweep run16:
      StoreContentStateCache::UpdateContentStateFromFastEnum 97.7->97.8%

--- src/system/meta/StorePackedMetadata.cpp
+++ src/system/meta/StorePackedMetadata.cpp
@@ -1335,10 +1335,9 @@ void StoreContentStateCache::PollUpdate() {
 }
 
 void StoreContentStateCache::UpdateContentStateFromFastEnum() {
-    int &_ref0 = mIndexInConfig;
     if (0 != TheWiiCommerceMgr.unk2110) {
         StoreTitleContentState *state;
-        unsigned long long titleId = TheWiiCommerceMgr.mTitleIds[_ref0];
+        unsigned long long titleId = TheWiiCommerceMgr.mTitleIds[mIndexInConfig];
         std::map<unsigned long long, StoreTitleContentState *>::iterator it = find(titleId);
         if (it == end()) {
             state = (StoreTitleContentState *)operator new(0x1000);
@@ -1363,7 +1362,7 @@ void StoreContentStateCache::UpdateContentStateFromFastEnum() {
             catalog += 0x10;
         }
     }
-    _ref0 += 1;
+    mIndexInConfig += 1;
 }
 
 void StoreMetadataManager::MarkPurchased(ECContentCatalogInfo *info) {
