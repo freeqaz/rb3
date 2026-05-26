ca64bc2ef7a61972b3a6e0fcf415383cce85951e
freeqaz <me@freeqaz.com>
2026-05-26 18:11:33 +0000

    decomp: RndFont::Load 98.0->98.1%
    permuter sweep run16:
      RndFont::Load 98.0->98.1%

--- src/system/rndobj/Font.cpp
+++ src/system/rndobj/Font.cpp
@@ -429,7 +429,7 @@ BEGIN_LOADS(RndFont)
     if (gRev > 0xD) {
         bs >> mTexCellSize;
         if (gRev < 0x11) {
-            for (int i = 0.0f; i < 0x100; i++) {
+            for (int i = 0; i < 0x100; i++) {
                 CharInfo &info = mCharInfoMap[i];
                 bs >> info.unk0;
                 bs >> info.unk4;
