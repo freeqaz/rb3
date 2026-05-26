ded6ee49131e867b6592dd8d27cbc1edac5c69e0
freeqaz <me@freeqaz.com>
2026-05-26 18:11:33 +0000

    decomp: WorldInstance::SyncDir 95.4->96.6%
    permuter sweep run16:
      WorldInstance::SyncDir 95.4->96.6%

--- src/system/world/Instance.cpp
+++ src/system/world/Instance.cpp
@@ -306,7 +306,7 @@ void WorldInstance::SyncDir() {
         DeleteTransientObjects();
         mSharedGroup = nullptr;
         if (mDir) {
-            RndGroup *grp = mDir->Find<RndGroup>("shared.grp", 0.0f);
+            RndGroup *grp = mDir->Find<RndGroup>("shared.grp", 0);
             if (!mDir->mSharedGroup2 && grp) {
                 mDir->mSharedGroup2 = new SharedGroup(grp);
             }
